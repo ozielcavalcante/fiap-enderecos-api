14a0ab6ac003b471d84651d851a1d77d79ab5348
Oziel Alves Cavalcante <oziel@tjro.jus.br>
2023-06-30 11:10:06 -0400

    adiciona configuração inicial

---
+++ src/main/java/monitorenergia/enderecosapi/EnderecosApiApplication.java
@@ -0,0 +1,13 @@
+package monitorenergia.enderecosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnderecosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnderecosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/monitorenergia/enderecosapi/EnderecosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package monitorenergia.enderecosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnderecosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
