f127cf5b4b2f5c24bf67f3555eae600448f38969
Oziel Alves Cavalcante <oziel@tjro.jus.br>
2023-09-03 15:00:48 -0400

    busca endereços por exemplo

--- src/main/java/monitorenergia/enderecosapi/controllers/EnderecoController.java
+++ src/main/java/monitorenergia/enderecosapi/controllers/EnderecoController.java
@@ -1,10 +1,14 @@
 package monitorenergia.enderecosapi.controllers;
 
 import lombok.AllArgsConstructor;
+import monitorenergia.enderecosapi.domain.Endereco;
 import monitorenergia.enderecosapi.dtos.EnderecoDTO;
 import monitorenergia.enderecosapi.repository.EnderecoRepository;
+import org.springframework.data.domain.Example;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("enderecos")
@@ -17,5 +21,11 @@ public class EnderecoController {
     public String novo(@RequestBody @Validated EnderecoDTO enderecoDTO) {
         enderecoRepository.save(enderecoDTO.toEntity());
         return "endereço OK";
+    }
+
+    @GetMapping
+    public List<Endereco> get(@RequestBody EnderecoDTO enderecoDTO) {
+        Example<Endereco> example = Example.of(enderecoDTO.toEntity());
+        return enderecoRepository.findAll(example);
     }
 }
